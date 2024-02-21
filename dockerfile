7f7f8cf6cc9d108fe7e16f8103aa186efaef99e7
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-02-21 21:09:00 +0100

    Main update
    Adds a single entry point script. Replaces etc-files with container image directives. Adds a Docker ignore file. Replaces the generate script with entry point commands. Adds a user removal command. Updates a container base image.

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM bitnami/minideb:bullseye
+FROM docker.io/bitnami/minideb:bookworm
 
 LABEL org.opencontainers.image.source https://github.com/Aeron/socks5-dante-proxy
 LABEL org.opencontainers.image.licenses MIT
@@ -8,21 +8,23 @@ RUN install_packages \
     openssl \
     curl
 
-ARG USER=socks
+WORKDIR /srv
+COPY . .
+
+RUN echo 'root:x:0:' > /etc/group && \
+    echo 'nogroup:x:65534:' >> /etc/group
+RUN echo 'root:*:17885:0:99999:7:::' > /etc/shadow && \
+    echo 'nobody:*:17885:0:99999:7:::' >> /etc/shadow
+RUN echo 'root:x:0:0:root:/root:/bin/bash' > /etc/passwd && \
+    echo 'nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin' >> /etc/passwd
 
 ENV WORKERS 4
 ENV CONFIG /etc/sockd.conf
 
-COPY etc/passwd /etc/passwd
-COPY etc/passwd /etc/shadow
-COPY etc/group /etc/group
-
-COPY dante.conf ${CONFIG}
-COPY generate.sh .
-
-RUN adduser --system --no-create-home ${USER}
+RUN ln -sf /srv/dante.conf "${CONFIG}"
 
 VOLUME /etc
 EXPOSE 1080
 
-CMD danted -N ${WORKERS} -f ${CONFIG}
+ENTRYPOINT [ "/srv/entrypoint.sh" ]
+CMD [ "start" ]
---
+++ entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+_strip_name() {
+    echo "$1" | tr '@&+.:' '-' | tr -d '=!%^#$\/()[]{}|;<>, ' | xargs
+}
+
+set -- "$1" "$(_strip_name "$2")"
+if [ -z "$2" ]; then
+    set -- "$1" 'socks'
+fi
+
+# shellcheck disable=SC2016
+HELP='Usage: /entrypoint.sh [COMMAND [PARAMS..]]
+
+Commands:
+    add-user NAME [PASS]    Add a new user
+    del-user NAME           Delete an existing user
+    start                   Start the dante server
+                            [container command]
+
+Parameters:
+    NAME                    A username
+                            [default: "socks"]
+'
+
+case "$1" in
+    'add-user')
+        USER="$2"
+        PASS=$(echo "$3" | xargs)
+        if [ -z "$PASS" ]; then
+            PASS=$(openssl rand -base64 16)
+        fi
+
+        adduser --quiet --system --no-create-home "$USER"
+        echo "$USER:$PASS" | chpasswd
+
+        URL="http://ifconfig.co"
+        HOST=$(curl -s "$URL")
+
+        echo 'SOCKS5 connection parameters:'
+        echo "- Server:   $HOST:1080"
+        echo "- Username: $USER"
+        echo "- Password: $PASS"
+        echo
+        echo 'Test it using the following command:'
+        echo "curl --socks5 $USER:$PASS@$HOST:1080 -L $URL"
+        ;;
+    'del-user')
+        deluser --quiet --system "$2" 2> /dev/null
+        ;;
+    'start')
+        danted -N "$WORKERS" -f "$CONFIG"
+        ;;
+    *)
+        echo "$HELP"
+        ;;
+esac
